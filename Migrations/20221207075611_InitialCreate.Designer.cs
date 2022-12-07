d7522872882d2a2b549af9d7d2e9c56c5098105e
Katje Kaczmarek <kkkaczmarek1@buffs.wtamu.edu>
2022-12-06 23:56:49 -0800

    c

--- Migrations/20221207075611_InitialCreate.Designer.cs
+++ Migrations/20221207075611_InitialCreate.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using CIDM3312_FinalProject.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CIDM3312FinalProject.Migrations
 {
     [DbContext(typeof(ProjectDbContext))]
-    partial class ProjectDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221207075611_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.0");
--- Models/CollectionLayer.cs
+++ Models/CollectionLayer.cs
@@ -8,6 +8,8 @@ namespace CIDM3312_FinalProject.Models
     {
         public int CollectionLayerId {get; set;}  //Facility Primary Key
         [Required]
+        [MinLength(2)]
+        [MaxLength(25)]
         public string CollectionCode {get; set;} = string.Empty;
         public string CollectionLabel {get; set;} = string.Empty;
         public List<FacilityCollection> FacilityCollections {get; set;} = default!; //Navigation to Collections by Facility
@@ -15,6 +17,8 @@ namespace CIDM3312_FinalProject.Models
 
     public class FacilityCollection
     {
+        [Key]
+        public int FcRowId {get; set;}  //Primary Key
         public int CollectionLayerId {get; set;} //PK,FK1 Composite
         public int FacilityId {get; set;} //PK, FK2 Composite
         public DateOnly ParticipationStartDate {get; set;}
