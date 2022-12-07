e23d13991605b880e0c85d1ea76b8586936ddc8e
Katje Kaczmarek <kkkaczmarek1@buffs.wtamu.edu>
2022-12-06 18:23:55 -0800

    c.

--- Models/Facility.cs
+++ Models/Facility.cs
@@ -5,9 +5,9 @@ namespace CIDM3312_FinalProject.Models
     public class Facility
     {
         public int FacilityId {get; set;}  //Facility Primary Key
-        public string FacilityCode {get; set;}
-        public string FacilityName {get; set;}
+        public string FacilityCode {get; set;} = string.Empty;
+        public string FacilityName {get; set;} = string.Empty;
         public int GwtgFacilityId {get; set;}
-        
+
     }
 }
