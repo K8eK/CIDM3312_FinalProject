86e17fb92992f925a85212a940dce2f896cf15c5
Katje Kaczmarek <kkkaczmarek1@buffs.wtamu.edu>
2022-12-06 23:50:31 -0800

    c

---
+++ Models/CollectionLayer.cs
@@ -0,0 +1,25 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace CIDM3312_FinalProject.Models
+{
+    public class CollectionLayer
+    {
+        public int CollectionLayerId {get; set;}  //Facility Primary Key
+        [Required]
+        public string CollectionCode {get; set;} = string.Empty;
+        public string CollectionLabel {get; set;} = string.Empty;
+        public List<FacilityCollection> FacilityCollections {get; set;} = default!; //Navigation to Collections by Facility
+    }
+
+    public class FacilityCollection
+    {
+        public int CollectionLayerId {get; set;} //PK,FK1 Composite
+        public int FacilityId {get; set;} //PK, FK2 Composite
+        public DateOnly ParticipationStartDate {get; set;}
+        public DateOnly ParticipationEndDate {get; set;}
+        public CollectionLayer CollectionLayer {get; set;} = default!; //Navigation to CollectionLayer
+        public Facility Facility {get; set;} = default!; //Navigation to Facility
+    }
+}
--- Models/Facility.cs
+++ Models/Facility.cs
@@ -1,13 +1,24 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace CIDM3312_FinalProject.Models
 {
     public class Facility
     {
         public int FacilityId {get; set;}  //Facility Primary Key
+        
+        [Display(Name = "Facility Code")]
+        [Required]
         public string FacilityCode {get; set;} = string.Empty;
+
+        [Display(Name = "Facility Name")]
+        [Required]
         public string FacilityName {get; set;} = string.Empty;
+
+        [Required]
         public int GwtgFacilityId {get; set;}
 
+        public List<FacilityCollection>? FacilityCollections {get; set;} = default!; //Navigation to Collections by Facility
     }
 }
