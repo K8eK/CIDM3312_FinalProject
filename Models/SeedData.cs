0480cea53f0e20b71554a9f10f5932771af15fda
Katje Kaczmarek <kkkaczmarek1@buffs.wtamu.edu>
2022-12-07 21:24:02 -0800

    d

--- Models/SeedData.cs
+++ Models/SeedData.cs
@@ -14,7 +14,8 @@ namespace CIDM3312_FinalProject.Models
                     return;
                 }
 
-                List<Facility> SeedFacilities = new List<Facility> {
+                List<Facility> SeedFacilities = new List<Facility>
+                {
                     new Facility {FacilityCode="BA", FacilityName="Swedish Ballard", GwtgFacilityId = 96580},
                     new Facility {FacilityCode="CH", FacilityName="Swedish Cherry Hill", GwtgFacilityId = 16002},
                     new Facility {FacilityCode="EDM", FacilityName="Swedish Edmonds", GwtgFacilityId = 16802},
@@ -25,7 +26,8 @@ namespace CIDM3312_FinalProject.Models
                 };
                 context.AddRange(SeedFacilities);
 
-                List<CollectionLayer> SeedCollectionLayers = new List<CollectionLayer> {
+                List<CollectionLayer> SeedCollectionLayers = new List<CollectionLayer>
+                {
                     new CollectionLayer {CollectionCode = "", CollectionLabel = ""},
                     new CollectionLayer {CollectionCode = "FRM_AR", CollectionLabel = "Arkansas"},
                     new CollectionLayer {CollectionCode = "FRM_ASR", CollectionLabel = "TJC ASR"},
@@ -78,83 +80,6 @@ namespace CIDM3312_FinalProject.Models
                     new CollectionLayer {CollectionCode = "FRM_FUP_TSC", CollectionLabel = "Stroke Follow-up: TSC"}
                 };
                 context.AddRange(SeedCollectionLayers);
-
-                List<FacilityCollection> SeedParticipation = new List<FacilityCollection> {
-                    new FacilityCollection {FacilityId = 0, CollectionLayerId = 0},
-                    new FacilityCollection {FacilityId = 1, CollectionLayerId = 2},
-                    new FacilityCollection {FacilityId = 1, CollectionLayerId = 4},
-                    new FacilityCollection {FacilityId = 1, CollectionLayerId = 7},
-                    new FacilityCollection {FacilityId = 1, CollectionLayerId = 8},
-                    new FacilityCollection {FacilityId = 1, CollectionLayerId = 21},
-                    new FacilityCollection {FacilityId = 1, CollectionLayerId = 26},
-                    new FacilityCollection {FacilityId = 1, CollectionLayerId = 27},
-                    new FacilityCollection {FacilityId = 1, CollectionLayerId = 28},
-                    new FacilityCollection {FacilityId = 1, CollectionLayerId = 32},
-                    new FacilityCollection {FacilityId = 1, CollectionLayerId = 37},
-                    new FacilityCollection {FacilityId = 2, CollectionLayerId = 4},
-                    new FacilityCollection {FacilityId = 2, CollectionLayerId = 6},
-                    new FacilityCollection {FacilityId = 2, CollectionLayerId = 7},
-                    new FacilityCollection {FacilityId = 2, CollectionLayerId = 8},
-                    new FacilityCollection {FacilityId = 2, CollectionLayerId = 9},
-                    new FacilityCollection {FacilityId = 2, CollectionLayerId = 12},
-                    new FacilityCollection {FacilityId = 2, CollectionLayerId = 21},
-                    new FacilityCollection {FacilityId = 2, CollectionLayerId = 26},
-                    new FacilityCollection {FacilityId = 2, CollectionLayerId = 27},
-                    new FacilityCollection {FacilityId = 2, CollectionLayerId = 29},
-                    new FacilityCollection {FacilityId = 2, CollectionLayerId = 32},
-                    new FacilityCollection {FacilityId = 3, CollectionLayerId = 4},
-                    new FacilityCollection {FacilityId = 3, CollectionLayerId = 7},
-                    new FacilityCollection {FacilityId = 3, CollectionLayerId = 8},
-                    new FacilityCollection {FacilityId = 3, CollectionLayerId = 12},
-                    new FacilityCollection {FacilityId = 3, CollectionLayerId = 21},
-                    new FacilityCollection {FacilityId = 3, CollectionLayerId = 23},
-                    new FacilityCollection {FacilityId = 3, CollectionLayerId = 26},
-                    new FacilityCollection {FacilityId = 3, CollectionLayerId = 27},
-                    new FacilityCollection {FacilityId = 3, CollectionLayerId = 29},
-                    new FacilityCollection {FacilityId = 3, CollectionLayerId = 32},
-                    new FacilityCollection {FacilityId = 3, CollectionLayerId = 37},
-                    new FacilityCollection {FacilityId = 4, CollectionLayerId = 4},
-                    new FacilityCollection {FacilityId = 4, CollectionLayerId = 7},
-                    new FacilityCollection {FacilityId = 4, CollectionLayerId = 8},
-                    new FacilityCollection {FacilityId = 4, CollectionLayerId = 12},
-                    new FacilityCollection {FacilityId = 4, CollectionLayerId = 21},
-                    new FacilityCollection {FacilityId = 4, CollectionLayerId = 23},
-                    new FacilityCollection {FacilityId = 4, CollectionLayerId = 26},
-                    new FacilityCollection {FacilityId = 4, CollectionLayerId = 27},
-                    new FacilityCollection {FacilityId = 4, CollectionLayerId = 32},
-                    new FacilityCollection {FacilityId = 4, CollectionLayerId = 37},
-                    new FacilityCollection {FacilityId = 5, CollectionLayerId = 4},
-                    new FacilityCollection {FacilityId = 5, CollectionLayerId = 7},
-                    new FacilityCollection {FacilityId = 5, CollectionLayerId = 8},
-                    new FacilityCollection {FacilityId = 5, CollectionLayerId = 12},
-                    new FacilityCollection {FacilityId = 5, CollectionLayerId = 21},
-                    new FacilityCollection {FacilityId = 5, CollectionLayerId = 23},
-                    new FacilityCollection {FacilityId = 5, CollectionLayerId = 26},
-                    new FacilityCollection {FacilityId = 5, CollectionLayerId = 27},
-                    new FacilityCollection {FacilityId = 5, CollectionLayerId = 32},
-                    new FacilityCollection {FacilityId = 5, CollectionLayerId = 37},
-                    new FacilityCollection {FacilityId = 6, CollectionLayerId = 2},
-                    new FacilityCollection {FacilityId = 6, CollectionLayerId = 4},
-                    new FacilityCollection {FacilityId = 6, CollectionLayerId = 7},
-                    new FacilityCollection {FacilityId = 6, CollectionLayerId = 8},
-                    new FacilityCollection {FacilityId = 6, CollectionLayerId = 21},
-                    new FacilityCollection {FacilityId = 6, CollectionLayerId = 26},
-                    new FacilityCollection {FacilityId = 6, CollectionLayerId = 27},
-                    new FacilityCollection {FacilityId = 6, CollectionLayerId = 28},
-                    new FacilityCollection {FacilityId = 6, CollectionLayerId = 32},
-                    new FacilityCollection {FacilityId = 6, CollectionLayerId = 37},
-                    new FacilityCollection {FacilityId = 7, CollectionLayerId = 2},
-                    new FacilityCollection {FacilityId = 7, CollectionLayerId = 4},
-                    new FacilityCollection {FacilityId = 7, CollectionLayerId = 7},
-                    new FacilityCollection {FacilityId = 7, CollectionLayerId = 8},
-                    new FacilityCollection {FacilityId = 7, CollectionLayerId = 21},
-                    new FacilityCollection {FacilityId = 7, CollectionLayerId = 26},
-                    new FacilityCollection {FacilityId = 7, CollectionLayerId = 27},
-                    new FacilityCollection {FacilityId = 7, CollectionLayerId = 28},
-                    new FacilityCollection {FacilityId = 7, CollectionLayerId = 32},
-                    new FacilityCollection {FacilityId = 7, CollectionLayerId = 37}
-                };
-                context.AddRange(SeedParticipation);
 
                 context.SaveChanges();
             }
