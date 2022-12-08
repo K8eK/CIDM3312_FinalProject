bbc56d457010a805becbe93a087fbba01fb8cc6d
Katje Kaczmarek <kkkaczmarek1@buffs.wtamu.edu>
2022-12-08 00:35:29 -0800

    d

--- Models/CollectionLayer.cs
+++ Models/CollectionLayer.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations;
 
 
--- Models/Facility.cs
+++ Models/Facility.cs
@@ -1,6 +1,7 @@
 using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations;
-
 
 namespace CIDM3312_FinalProject.Models
 {
